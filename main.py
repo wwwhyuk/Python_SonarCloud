6c4f4d972ab21c8b824fc4902f1dde6964c1a09c
wwwhyuk <joohyeok5118@naver.com>
2024-05-23 16:00:03 +0900

    first

--- main.py
+++ main.py
@@ -8,26 +8,26 @@ def print_hi(name):
 
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    prinsdt(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    prisdnt(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    prsdint(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    prsdint(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('wwwhyuk')
-    print_hi('wsws')
+    prinsst_hi('wsws')
     print_hi('wsws')
 
+    print_sdsshi('this')
     print_hi('this')
     print_hi('this')
+    prsdint_hi('this')
+    prinacacact_hi('this')
     print_hi('this')
-    print_hi('this')
-    print_hi('this')
-    print_hi('this')
-    print_hi('this')
+    princcacact_hi('this')
     print_hi('this')
     print_hi('this')
     print_hi('this')
