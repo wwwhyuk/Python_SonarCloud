a111b01d370de7e3a68ccb5eb2ce0c879eb2f9a5
wwwhyuk <joohyeok5118@naver.com>
2024-05-23 16:06:30 +0900

    first

--- main.py
+++ main.py
@@ -1,43 +1,5 @@
-# This is a sample Python script.
-
-# Press Ctrl+Enter to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(name):
+    print(f'Hi, {name}')
 
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    prinsdt(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    prisdnt(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    prsdint(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    prsdint(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('wwwhyuk')
-    prinsst_hi('wsws')
-    print_hi('wsws')
-
-    print_sdsshi('this')
-    print_hi('this')
-    print_hi('this')
-    prsdint_hi('this')
-    prinacacact_hi('this')
-    print_hi('this')
-    princcacact_hi('this')
-    print_hi('this')
-    print_hi('this')
-    print_hi('this')
-    print_hi('this')
-    print_hi('this')
-    print_hi('this')
-    print_hi('this')
-    print_hi('this')
-    print_hi('this')
-    print_hi('this')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-"C:\Users\rkfpt\Downloads\sonar-scanner-cli-5.0.1.3006-windows\sonar-scanner-5.0.1.3006-windows\bin"
