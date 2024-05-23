a7adeb3a3ff783bfddcb953d0c442da1902365fa
wwwhyuk <joohyeok5118@naver.com>
2024-05-23 15:39:43 +0900

    first

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Ctrl+Enter to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('wwwhyuk')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+"C:\Users\rkfpt\Downloads\sonar-scanner-cli-5.0.1.3006-windows\sonar-scanner-5.0.1.3006-windows\bin"
